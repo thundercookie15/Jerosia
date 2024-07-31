4cde788714352b235684fc56f6f601e998fb6ab7
thundercookie15 <lucas.maijers@student.rocvf.nl>
2024-07-31 14:27:28 +0200

    Basics for Spells

--- src/main/java/nl/jerosia/wands/magic/ISpell.java
+++ src/main/java/nl/jerosia/wands/magic/ISpell.java
@@ -1,4 +1,17 @@
 package nl.jerosia.wands.magic;
 
+import nl.jerosia.player.User;
+
 public interface ISpell {
+
+    void fire(User user);
+    int getSpellId();
+    String getSpellName();
+
+    default void startCasting(User user) {}
+    default void stopCasting(User user) {}
+    default boolean isCastingSpell(User user) {return false;}
+    default boolean isToggleSpell() {
+        return false;
+    }
 }
